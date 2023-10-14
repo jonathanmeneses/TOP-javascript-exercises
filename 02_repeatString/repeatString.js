57a3ebfa36dcfedb0215415379fe8e436bc03017
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-10-14 16:40:44 -0500

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+let stringToReturn = '';
+if (typeof(num) == 'number' && num >= 0) {
+    for (let i = 0; i < num; i++) {
+        stringToReturn += string
+    }
+    return stringToReturn
+}
+else {
+    return 'ERROR'
+}
+
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
