cf8b99dd5f0f4d21a5068da68db2844fb47902d1
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-10-15 16:28:47 -0500

    solve challenges

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let CTemp;
+
+  CTemp = (temp - 32) * 5 / 9
+
+  return Number(CTemp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let FTemp;
+
+  FTemp = (temp * 9/5) +32
+
+  return Number(FTemp.toFixed(1))
 };
+
 
 // Do not edit below this line
 module.exports = {
