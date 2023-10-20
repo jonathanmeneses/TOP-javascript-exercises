200c65ecfa7a6a1fd2809c961bfa08b1de3e1214
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-10-19 21:14:37 -0500

    solved challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    function isPunctuation(item) {
+        const punctuationMarks = ["!", ",", ".", "?"," "];
+        return punctuationMarks.includes(item);
+      }
+    
+    str = str.toLowerCase()
+    const split_string = str.split("")
+    const filteredStr= split_string.filter(element => !isPunctuation(element));
+
+    
+    const reverse_array = filteredStr.slice().reverse()
+
+    console.log(filteredStr)
+    console.log(reverse_array)
+    for (let i=0; i < split_string.length; i++) {
+        if (filteredStr[i] != reverse_array[i]) {
+            return false
+        }
+    }
+    return true
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
